6d5bf0240600943228ae862f77a3294edf1e21ca
Youssef434 <fatmi2019py@gmail.com>
2023-12-24 19:43:47 +0100

    implement the score method

--- src/main/java/services/ScoreService.java
+++ src/main/java/services/ScoreService.java
@@ -1,9 +1,12 @@
 package services;
 
+import cards.Card;
 import players.Player;
 
+import java.util.Map;
+
 public interface ScoreService {
-  int countScore(Player player);
+  double countScore(Player player);
 
   static ScoreService create() {
     return new ScoreServiceImpl();
@@ -11,9 +14,25 @@ public interface ScoreService {
 }
 
 class ScoreServiceImpl implements ScoreService {
+  private final Map<Integer, Double> scoreMap;
+
+  public ScoreServiceImpl() {
+    scoreMap = Map.of(
+        3, .33,
+        2, .33,
+        1, 1d,
+        12, .33,
+        11, .33,
+        10, .33);
+  }
 
   @Override
-  public int countScore(Player player) {
-    return 0;
+  public double countScore(Player player) {
+    return player.cards()
+        .stream()
+        .map(Card::getNumber)
+        .filter(scoreMap::containsKey)
+        .mapToDouble(scoreMap::get)
+        .sum();
   }
 }
