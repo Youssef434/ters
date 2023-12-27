0cff70458b089fed1bf45510098163bf22e889d4
Youssef434 <fatmi2019py@gmail.com>
2023-12-27 01:32:49 +0100

    Add one to the one who jab l9a3a hhhh

--- src/main/java/game/Round.java
+++ src/main/java/game/Round.java
@@ -7,10 +7,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public record Round(List<Cycle.CycleResult> cycleResults, ScoreService scoreService) {
+public record Round(List<Cycle.CycleResult> cycleResults, ScoreService scoreService, Team lastCycleWinner) {
   public Map<Team, Double> getScore() {
-    return cycleResults.stream()
+    var scoreMap = cycleResults.stream()
         .map(scoreService::countCycleScore)
         .collect(Collectors.groupingBy(TeamScore::team, Collectors.summingDouble(TeamScore::score)));
+    scoreMap.compute(lastCycleWinner, (k, v) -> v + 1);
+    return scoreMap;
   }
 }
