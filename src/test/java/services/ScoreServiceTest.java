6eba45a2ad726b88ba27042a7fa58f621a256a48
Youssef434 <fatmi2019py@gmail.com>
2024-01-03 20:42:22 +0100

    [pom.xml] write the first parametrized csv test for countScore in ScoreService.

--- src/test/java/services/ScoreServiceTest.java
+++ src/test/java/services/ScoreServiceTest.java
@@ -2,8 +2,13 @@ package services;
 
 import cards.Card;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static cards.CardType.*;
@@ -29,7 +34,22 @@ public class ScoreServiceTest {
         Card.of(5, BASTOS),
         Card.of(6, BASTOS),
         Card.of(7, BASTOS));
+
     var score = scoreService.countScore(cards);
+
     assertEquals(0d, score);
+  }
+
+  @ParameterizedTest
+  @CsvSource({"1345,1.34"})
+  public void testGloballyCountScore(String cardNumbers, String expectedScore) {
+    var cards = Arrays.stream(cardNumbers.split(""))
+        .map(Integer::parseInt)
+        .map(cardNum -> Card.of(cardNum, OROS))
+        .toList();
+
+    var score = scoreService.countScore(cards);
+
+    assertEquals(score, Double.parseDouble(expectedScore));
   }
 }
