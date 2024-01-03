2d8f630f3397c0331d7c588cec7c12677ec3dc27
Youssef434 <fatmi2019py@gmail.com>
2024-01-03 20:23:59 +0100

    [tests] test the countScore method when cards is empty.

--- src/test/java/services/ScoreServiceTest.java
+++ src/test/java/services/ScoreServiceTest.java
@@ -1,5 +1,22 @@
 package services;
 
-public class ScoreServiceTest {
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScoreServiceTest {
+  private static ScoreService scoreService;
+
+  @BeforeAll
+  public static void init() {
+    scoreService = ScoreService.create();
+  }
+
+  @Test
+  public void testCountScoreOfEmptyCardsIsZero() {
+    assertEquals(scoreService.countScore(List.of()), 0d);
+  }
 }
