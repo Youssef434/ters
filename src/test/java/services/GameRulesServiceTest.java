81380321df4dfed1be8107954b179327e2405730
Youssef434 <fatmi2019py@gmail.com>
2024-01-05 23:17:12 +0100

    [tests] test playable cards when hand is empty.

--- src/test/java/services/GameRulesServiceTest.java
+++ src/test/java/services/GameRulesServiceTest.java
@@ -1,4 +1,27 @@
 package services;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import players.Player;
+import players.Team;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameRulesServiceTest {
+  private static GameRulesService gameRulesService;
+
+  @BeforeAll
+  public static void init() {
+    gameRulesService = GameRulesService.create();
+  }
+  @Test
+  public void testPlayerWithEmptyHand() {
+    var player = Player.of(Team.A, "", Set.of());
+
+    var playableCards = gameRulesService.playableCards(player, null);
+
+    assertEquals(Set.of(), playableCards);
+  }
 }
