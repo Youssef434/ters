1f52f78b5b257b7e342c4ac9d2bd3727b6c07d8f
Youssef434 <fatmi2019py@gmail.com>
2024-01-06 21:11:23 +0100

    [tests] test when we need to redistribute the cards.

---
+++ src/test/java/services/GameEligibilityServiceTest.java
@@ -0,0 +1,60 @@
+package services;
+
+import cards.Card;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import players.Player;
+import players.Team;
+
+import java.util.List;
+import java.util.Set;
+
+import static cards.CardType.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameEligibilityServiceTest {
+  private static GameEligibilityService gameEligibilityService;
+
+  @BeforeAll
+  public static void init() {
+    gameEligibilityService = GameEligibilityService.create();
+  }
+
+  @Test
+  public void testPlayerHavingMoreThan6CardsOfTheSameType() {
+    var players = List.of(Player.of(Team.A, "", Set.of(
+        Card.of(1, BASTOS),
+        Card.of(2, BASTOS),
+        Card.of(3, BASTOS),
+        Card.of(12, BASTOS),
+        Card.of(11, BASTOS),
+        Card.of(10, BASTOS),
+        Card.of(4, BASTOS),
+        Card.of(3, OROS),
+        Card.of(1, COPAS),
+        Card.of(1, ESPADAS))));
+
+     boolean isEligible = gameEligibilityService.isEligible(players);
+
+    assertFalse(isEligible);
+  }
+
+  @Test
+  public void testPlayerHavingLessThanFourThirdsInHisHand() {
+    var players = List.of(Player.of(Team.A, "", Set.of(
+        Card.of(5, BASTOS),
+        Card.of(6, BASTOS),
+        Card.of(7, BASTOS),
+        Card.of(12, BASTOS),
+        Card.of(11, BASTOS),
+        Card.of(10, BASTOS),
+        Card.of(4, BASTOS),
+        Card.of(4, OROS),
+        Card.of(4, COPAS),
+        Card.of(4, ESPADAS))));
+
+    boolean isEligible = gameEligibilityService.isEligible(players);
+
+    assertFalse(isEligible);
+  }
+}
